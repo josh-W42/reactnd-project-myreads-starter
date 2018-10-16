ce337b26c1a257c87d1eed53710c8c023b6d3f6e
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-10-16 07:23:38 -0700

    added an additional component to main page for previewing a book for more information

---
+++ src/BookPreview.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+class BookPreview extends Component {
+
+  render() {
+    let book = this.props.focusedBook;
+
+    return (
+      <div>
+        { book !== 0 && (
+          <div key={ book.title } className="book">
+            <div className="book-top">
+              <div className="book-cover" style={{ backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+              <div className="book-shelf-changer">
+                <select onClick={(e) => {
+                    this.props.onUpdate(book, e.target.value)
+                  }}>
+                  <option value="move" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+              </div>
+            </div>
+            <div className="book-info">
+              <h2>Title</h2>
+              <div className="book-title">{book.title}</div>
+              <h2>Author(s)</h2>
+              <div className="book-authors">{book.authors}</div>
+            </div>
+            <h2>Description</h2>
+            <div className="book-description">{book.description}</div>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+export default BookPreview
