d1cacc48fc89573e638bb567d47cc81bdcb59514
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-11-13 16:25:46 -0800

    added a link element to connect the search page back to the main shelf page.

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import * as BooksAPI from './BooksAPI'
+import { Link } from 'react-router-dom'
 
 class BookSearch extends Component {
 
@@ -35,7 +36,7 @@ class BookSearch extends Component {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => document.location.pathname = '/'}>Close</a>
+          <Link className="close-search" to="/">Close</Link>
           <div className="search-books-input-wrapper">
             {/*
               NOTES: The search from BooksAPI is limited to a particular set of search terms.
