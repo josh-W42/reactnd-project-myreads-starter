19f02cb4414509adf4286c208a34f534ec1fa1bb
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-11-13 16:24:47 -0800

    implimented react router dom routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react'
+import { Link, Route } from 'react-router-dom'
 import BookShelf from './BookShelf'
 import BookSearch from './BookSearch'
 import './App.css'
 
 class BooksApp extends Component {
 
+  state = {
+    screen: 'shelf',
+  }
+
   render() {
     return (
       <div className="app">
-        {document.location.pathname === '/search' ? (
-          <BookSearch />
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
+        <Route path="/search" render={() => (
+            <BookSearch
+            />
+          )}
+        />
+        <Route exact path="/" render={({ history }) => (
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <BookShelf
+                onNavigate={() => {
+                  this.setState({ screen: 'search' })
+                  history.push('/')
+                }}
+                />
+              <div className="open-search">
+                <Link className="close-create-contact" to="/search">Add a book</Link>
+              </div>
             </div>
-            <BookShelf />
-            <div className="open-search">
-              <a onClick={() => document.location.pathname = '/search'}>Add a book</a>
-            </div>
-          </div>
-        )}
+          )} />
       </div>
     )
   }
