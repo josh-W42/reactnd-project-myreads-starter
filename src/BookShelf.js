db764d205d708134e5a44b772ff0da65ee61b244
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-11-21 10:27:22 -0800

    reformated code for repetitions and updated code for new state management in appjs

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -7,14 +7,39 @@ class BookShelf extends Component {
 
   state = {
     currentBook: 0,
-
     books: [],
+    bookShelves: this.props.bookShelves,
   }
 
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
-      this.setState({ books: books });
-    })
+      let bookShelves = [
+        {
+          shelf: 'currentlyReading',
+          name: 'Currently Reading Books',
+          books: [],
+        },
+        {
+          shelf: 'wantToRead',
+          name: 'Books I Want to Read',
+          books: [],
+        },
+        {
+          shelf: 'read',
+          name: 'Books I Have Read',
+          books: [],
+        }
+      ];
+
+      bookShelves.forEach((bookShelf) => {
+        books.filter((book) => book.shelf === bookShelf.shelf).forEach((book) => {
+          bookShelf['books'].push(book);
+        });
+      });
+
+      this.setState({ bookShelves, books });
+      this.props.intialization(bookShelves, books);
+    });
   }
 
   render() {
@@ -23,107 +48,45 @@ class BookShelf extends Component {
       <main>
         <div className="list-books-content">
           <div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Currently Reading</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                  {
-                    this.state.books.filter( (book) => book.shelf === 'currentlyReading').map( (book) => (
-                      <li key={ book.id }>
-                        <div className="book" onClick={() => this.setState({ currentBook: book })}>
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                            <div className="book-shelf-changer">
-                              <select value={book.shelf} onClick={(e) => {
-                                  BooksAPI.update(book, `${e.target.value}`).then(this.componentDidMount())
-                                }}>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
+            {
+              this.state.bookShelves.map((bookShelf) => (
+                <div className="bookshelf" key={bookShelf.shelf}>
+                  <h2 className="bookshelf-title">{bookShelf.name}</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      {
+                        bookShelf.books.map( (book) => (
+                          <li key={ book.id }>
+                            <div className="book" onClick={() => this.setState({ currentBook: book })}>
+                              <div className="book-top">
+                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                <div className="book-shelf-changer">
+                                  <select value={book.shelf} onChange={(e) => { this.props.bookShelfUpdate(e, book) }}>
+                                    <option value="move" disabled>Move to...</option>
+                                    <option value="currentlyReading">Currently Reading</option>
+                                    <option value="wantToRead">Want to Read</option>
+                                    <option value="read">Read</option>
+                                    <option value="none">None</option>
+                                  </select>
+                                </div>
+                              </div>
+                              <div className="book-title">{book.title}</div>
+                              <div className="book-authors">{book.authors}</div>
                             </div>
-                          </div>
-                          <div className="book-title">{book.title}</div>
-                          <div className="book-authors">{book.authors}</div>
-                        </div>
-                      </li>
-                    ))
-                  }
-                </ol>
-              </div>
-            </div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Want to Read</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                  {
-                     this.state.books.filter( (book) => book.shelf === 'wantToRead').map( (book) => (
-                      <li key={ book.id }>
-                        <div className="book" onClick={() => this.setState({ currentBook: book })}>
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                            <div className="book-shelf-changer">
-                              <select value={book.shelf} onClick={(e) => {
-                                  BooksAPI.update(book, `${e.target.value}`).then(this.componentDidMount())
-                                }}>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">{book.title}</div>
-                          <div className="book-authors">{book.authors}</div>
-                        </div>
-                      </li>
-                    ))
-                  }
-                </ol>
-              </div>
-            </div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Read</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                  {
-                    this.state.books.filter( (book) => book.shelf === 'read').map( (book) => (
-                      <li key={ book.id }>
-                        <div className="book" onClick={() => this.setState({ currentBook: book })}>
-                          <div className="book-top">
-                            <div className="book-cover" style={{ backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                            <div className="book-shelf-changer">
-                              <select value={book.shelf} onClick={(e) => {
-                                  BooksAPI.update(book, `${e.target.value}`).then(this.componentDidMount())
-                                }}>
-                                <option value="move" disabled>Move to...</option>
-                                  <option value="currentlyReading">Currently Reading</option>
-                                  <option value="wantToRead">Want to Read</option>
-                                  <option value="read">Read</option>
-                                  <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">{book.title}</div>
-                          <div className="book-authors">{book.authors}</div>
-                        </div>
-                      </li>
-                    ))
-                  }
-                </ol>
-              </div>
-            </div>
+                          </li>
+                        ))
+                      }
+                    </ol>
+                  </div>
+                </div>
+              ))
+            }
           </div>
         </div>
         <div className="book-preview">
           <BookPreview
             focusedBook={this.state.currentBook}
-            onUpdate={(book, value) => {
-              BooksAPI.update(book, `${value}`).then(this.componentDidMount())
-            }} />
+            onUpdate={(e, book) => this.props.bookShelfUpdate(e, book) } />
         </div>
       </main>
     )
