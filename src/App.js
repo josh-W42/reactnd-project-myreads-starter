10ec38fcde3c68b3a51917a4acecd1897f280fb8
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-11-21 10:22:45 -0800

    reformated how shelf management is controled by App state, updates from lower components are pushed through Appjs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import * as BooksAPI from './BooksAPI'
 import { Link, Route } from 'react-router-dom'
 import BookShelf from './BookShelf'
 import BookSearch from './BookSearch'
@@ -8,27 +9,45 @@ class BooksApp extends Component {
 
   state = {
     screen: 'shelf',
+    bookShelves: [],
+    books: [],
+  }
+
+  onShelfUpdate(e, book, updateType) {
+    BooksAPI.update(book, `${e.target.value}`);
+
+    if(updateType === 'bookshelf') {
+      let BookShelfBooks = this.state.bookShelves.filter((bookShelf) => bookShelf.shelf === book.shelf)[0]['books'];
+
+      const bookIndex = BookShelfBooks.indexOf(book);
+      BookShelfBooks.splice(bookIndex, 1);
+    }
+
+    book.shelf = e.target.value;
+    this.state.bookShelves.filter((bookShelf) => bookShelf.shelf === book.shelf)[0]['books'].push(book);
   }
 
   render() {
     return (
       <div className="app">
-        <Route path="/search" render={() => (
+        <Route path="/search"
+          render={() => (
             <BookSearch
-            />
-          )}
-        />
-        <Route exact path="/" render={({ history }) => (
+              books={this.state.books}
+              bookShelves={this.props.bookShelves}
+              bookShelfUpdate={ (e, book) => this.onShelfUpdate(e, book)}
+               />
+          )} />
+        <Route exact path="/" render={() => (
             <div className="list-books">
               <div className="list-books-title">
                 <h1>MyReads</h1>
               </div>
               <BookShelf
-                onNavigate={() => {
-                  this.setState({ screen: 'search' })
-                  history.push('/')
-                }}
-                />
+                bookShelves={this.state.bookShelves}
+                intialization={(bookShelves, books) => this.setState({ bookShelves, books })}
+                bookShelfUpdate={ (e, book) => this.onShelfUpdate(e, book, 'bookshelf')}
+              />
               <div className="open-search">
                 <Link className="close-create-contact" to="/search">Add a book</Link>
               </div>
