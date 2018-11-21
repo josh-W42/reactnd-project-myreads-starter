dd0bebeca2dae766f8976d9c549b863e2d7ce366
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-11-21 10:25:01 -0800

    removed the feature to change the active books shelf

--- src/BookPreview.js
+++ src/BookPreview.js
@@ -11,17 +11,6 @@ class BookPreview extends Component {
           <div key={ book.title } className="book">
             <div className="book-top">
               <div className="book-cover" style={{ backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-              <div className="book-shelf-changer">
-                <select onClick={(e) => {
-                    this.props.onUpdate(book, e.target.value)
-                  }}>
-                  <option value="move" disabled>Move to...</option>
-                  <option value="currentlyReading">Currently Reading</option>
-                  <option value="wantToRead">Want to Read</option>
-                  <option value="read">Read</option>
-                  <option value="none">None</option>
-                </select>
-              </div>
             </div>
             <div className="book-info">
               <h2>Title</h2>
